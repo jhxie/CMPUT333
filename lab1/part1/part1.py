58ae05a897f60f4b9701b761c0472178a88a603e
sarychuk <sarychuk@ualberta.ca>
2016-09-16 17:36:44 -0600

    Finished part 1 without touching up the functions or the manual key entry

--- lab1/part1/part1.py
+++ lab1/part1/part1.py
@@ -85,6 +85,9 @@ def cleanArrayForPlaintext(lst):
         elif i == 7:
             temp = [i, x[1]]
             array.append(temp)
+        elif i == 0:
+            temp = [i, x[1]]
+            array.append(temp)
     return array
 
 def cleanArrayForKey(lst):
@@ -130,11 +133,52 @@ def possibleKeys(cllst, chlst):
     keybyte = 0
     for i, x in enumerate(cllst):
         for j, y in enumerate(chlst):
-          keybyte = (x[1] << 4) + y[1]
-          keys.append(keybyte)
+          if (x[0] != 10) and (y[0] == 0):
+              continue
+          else:
+              keybyte = (x[1] << 4) + y[1]
+              keys.append(format(keybyte, "#04x"))
     return keys
 
-def main():
+def possiblePlaintext(chlst, cllst):
+    """
+    """
+    plaintext = []
+    plaintextbyte = 0
+    plaintextval = "a"
+    for i, x in enumerate(chlst):
+        for j, y in enumerate(cllst):
+          plaintextbyte = (x[0] << 4) + y[0]
+          plaintextval = chr(plaintextbyte)
+          plaintext.append(plaintextval)
+    return plaintext
+
+def checkKeysPerLetter(prevKeyslst, checkKeyslst):
+    """
+    This method will keep the keys that exist in both lists
+    """
+    keys = list(set(prevKeyslst).intersection(checkKeyslst))
+
+    return keys
+
+def plaintext(mapping, ch, cl, kh, kl):
+    for i, x in enumerate(mapping):
+        for j, y in enumerate(x):
+            if (j == kh) and (j == kl) and (mapping[i][kh] == cl) and (mapping[i][kl] == ch):
+                pl = i
+                ph = i
+            elif (j == kh) and (mapping[i][kh] == cl):
+                pl = i
+            elif (j == kl) and (mapping[i][kl] == ch):
+                ph = i
+
+    txt = str(chr((ph << 4) + pl))
+    return txt
+
+def test():
+    frstByte = True
+    maxPossKeys = []
+    newPossKeys = []
 
     cipherfn = ''
     plainfn = ''
@@ -152,31 +196,137 @@ def main():
     pfile = open(plainfn, "w")
 
     with open(cipherfn, "rb") as cipherfile:
-        byte = cipherfile.read(1)
+        i = 0
+        while True:
+            byte = cipherfile.read(1)
+            if not byte:
+                break
+            i += 1
 
-        intbyte = ord(byte)
+            intbyte = ord(byte)
 
-        ch = intbyte >> 4
-        cl = intbyte & 15
+            ch = intbyte >> 4
+            cl = intbyte & 15
 
-        # cut out unneccesary ph values
-        pPoss = findInDoubleMatrix(mapping, ch)
-        kPoss = findInDoubleMatrix(mapping, cl)
-        pClean = cleanArrayForPlaintext(pPoss)
-        kClean = cleanArrayForKey(kPoss)
+            if i == 1:
+                pchar = plaintext(mapping, ch, cl, 0x5, 0x0)
+            elif i == 2:
+                pchar = plaintext(mapping, ch, cl, 0x2, 0xf)  
+            elif i == 3:
+                pchar = plaintext(mapping, ch, cl, 0x0, 0x8)
+            elif i == 4:
+                pchar = plaintext(mapping, ch, cl, 0x7, 0xc)
+            elif i == 5:
+                pchar = plaintext(mapping, ch, cl, 0x5, 0xf)
+            elif i == 6:
+                pchar = plaintext(mapping, ch, cl, 0x3, 0x0)
+            elif i == 7:
+                pchar = plaintext(mapping, ch, cl, 0x0, 0x0)
+                i = 0
 
-        # possible keys for this byte
-        print("poss keys: ", possibleKeys(kClean, pClean))
-        print("len: ", len(possibleKeys(kClean, pClean)))
+            pfile.write(pchar)
 
-        print("ch =", format(ch, "#03x"))
-        print("cl =", format(cl, "#03x"))
-        # print(format(intbyte, '#04x')) use this to print as 0x## where the ##
-        # are any digit from 0-f
+    pfile.close()
+
+    return
+
+# add separation of key lengths  ( 7 different arrays )
+def main():
+
+    frstByte = True
+    maxPossKeys = []
+    newPossKeys = []
+
+    cipherfn = ''
+    plainfn = ''
+
+    if len(sys.argv) == 1:
+        print("No cipherfile input added")
+        sys.exit(2)
+    elif len(sys.argv) == 2:
+        print("No output file added")
+        sys.exit(2)
+    else:
+        cipherfn = sys.argv[1]
+        plainfn = sys.argv[2]
+
+    pfile = open(plainfn, "w")
+
+    with open(cipherfn, "rb") as cipherfile:
+        #i = 1;
+        while True:
+        #while i == 1:
+            #read first byte
+            #print()
+            if frstByte == True:
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+
+            #read every 7th byte afterwards
+            else:
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                byte = cipherfile.read(1)
+                i = 0
+
+            intbyte = ord(byte)
+
+            ch = intbyte >> 4
+            cl = intbyte & 15
+
+            keys = findKeys(ch, cl)
+
+            # cut out unneccesary ph values
+            pPoss = findInDoubleMatrix(mapping, ch)
+            kPoss = findInDoubleMatrix(mapping, cl)
+
+            ##print()
+            ##print(pPoss)
+            ##print(kPoss)
+
+            pClean = cleanArrayForPlaintext(pPoss)
+            kClean = cleanArrayForKey(kPoss)
+
+            ##print()
+            ##print(pClean)
+            ##print(kClean)
+
+            possKeys = sorted(possibleKeys(kClean, pClean))
+            possPlaintext = sorted(possiblePlaintext(pClean, kClean))
+
+            #print("possKeys: ", possKeys)
+            #print("possPlaintext: ", possPlaintext)
+            #print(format(intbyte, '#04x'))
+
+            if frstByte == True:
+                maxPossKeys = possKeys
+                frstByte = False
+                print("poss keys: ", maxPossKeys)
+            else:
+
+                maxPossKeys = checkKeysPerLetter(maxPossKeys, possKeys)
+      
+                # possible keys for this byte
+                print("poss keys: ", sorted(maxPossKeys))
+                print("len: ", len(maxPossKeys))
+
+                #print("ch =", format(ch, "#03x"))
+                #print("cl =", format(cl, "#03x"))
+                print(format(intbyte, '#04x')) #use this to print as 0x## where the ##
+                # are any digit from 0-f
 
     pfile.close()
 
     return
 
 if __name__ == "__main__":
-    main()
+    test()
