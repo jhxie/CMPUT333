5fd2a4226c2e33b7c40690546660301483c8e345
Jiahui Xie <jiahui.xie@outlook.com>
2016-09-29 15:07:52 -0600

    Add 'pandoc' checking directive in 'reportCompile.sh' of lab 1

--- lab1/reportCompile.sh
+++ lab1/reportCompile.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/sh
+#!/bin/bash
 
-report="REPORT.md"
-reportdoc="REPORT.pdf"
-report1="part1/REPORT.md"
-report2="part2/REPORT.md"
-report3="part3/REPORT.md"
+out="REPORT.pdf"
+in1="part1/REPORT.md"
+in2="part2/REPORT.md"
+in3="part3/REPORT.md"
 
-if [ -f "$report" ]; then
-    rm -rf "$report"
-fi
-
-if [[ -f "$report1" && -f "$report2" && -f "$report3" ]]; then
-    echo "[SUCCESS] The compiled result is generated."
-    cat "$report1" "$report2" "$report3" > "$report"
-    which pandoc && pandoc -f markdown_github "$report" -o "$reportdoc"
+if [[ -f "$in1" && -f "$in2" && -f "$in3" ]]; then
+    # if 'pandoc' is not found
+    if ! command -v pandoc > /dev/null 2>&1; then
+        echo "[FAIL] pandoc not found"
+    else
+        cat "$in1" "$in2" "$in3" | pandoc -f markdown_github -o "$out"
+        echo "[SUCCESS] The compiled result is generated."
+    fi
 else
     echo "[FAIL] Some of the input files are missing."
 fi
+
